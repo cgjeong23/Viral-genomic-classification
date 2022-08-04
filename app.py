25484322838bb9f332ac7a03816586f697309d1c
Sehyun Choi <choisehyun98@gmail.com>
2022-08-04 17:04:36 +0900

    use mock data

--- app.py
+++ app.py
@@ -15,21 +15,36 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 model_path = 'ML/model.pth'
-model, tokenizer, label_dict = load_for_inference(model_path, "ML/gene_tokenizer.json", "ML/label_dict.json",
-embedding_dim=256, hidden_dim=64, num_layers=1)
+model, tokenizer, label_dict = load_for_inference(model_path,
+                                                  "ML/gene_tokenizer.json",
+                                                  "ML/label_dict.json",
+                                                  embedding_dim=256,
+                                                  hidden_dim=64,
+                                                  num_layers=1)
 
-with open('ML/pca.pkl','rb') as f:
+with open('pca.pkl', 'rb') as f:
     pca = pickle.load(f)
 
 virus_embeddings = np.load('virus_embeddings_3d.npy')
-
 with open('label.pkl', 'rb') as f:
     label = pickle.load(f)
 
+label_index = {k: [] for k in set(label)}
+
+for i, l in enumerate(label):
+    label_index[l].append(i)
+
+label_index = {k: np.random.choice(v, 100) for k, v in label_index.items()}
+
+sampled_index = np.concatenate(list(label_index.values()))
+
+virus_embeddings = virus_embeddings[sampled_index]
+label = [label[i] for i in sampled_index]
+
 embedding_df = pd.DataFrame({
-    'component1': virus_embeddings[:,0],
-    'component2': virus_embeddings[:,1],
-    'component3': virus_embeddings[:,2],
+    'x': virus_embeddings[:, 0],
+    'y': virus_embeddings[:, 1],
+    'z': virus_embeddings[:, 2],
     'Virus Type': label,
 })
 
@@ -50,28 +65,34 @@ app.layout = html.Div([
     html.Button("Go!", id='go-button'),
     html.Br(),
     html.Div(id='my-output'),
-], style={'text-align':'center'})
+],
+                      style={'text-align': 'center'})
+
 
 def update_embedding_viz(input_value):
     emb = infer(input_value, tokenizer, model.embedding).numpy()
     emb_3d = pca.transform(emb)
 
     emb_df = pd.DataFrame({
-        'component1':emb_3d[:,0],
-        'component2':emb_3d[:,1],
-        'component3':emb_3d[:,2],
+        'component1': emb_3d[:, 0],
+        'component2': emb_3d[:, 1],
+        'component3': emb_3d[:, 2],
         'Virus Type': "NEW"
     })
 
     plot_df = pd.concat([embedding_df, emb_df])
-    fig = px.scatter_3d(plot_df, x='component1', y='component2', z='component3', color='Virus Type')
+    fig = px.scatter_3d(plot_df,
+                        x='x',
+                        y='y',
+                        z='z',
+                        color='Virus Type',
+                        title="Virus Feautres 3D visualization")
     return dcc.Graph(id='3d-viz', figure=fig)
 
 
 @app.callback(Output(component_id='my-output', component_property='children'),
               State(component_id='my-input', component_property='value'),
               Input(component_id='go-button', component_property='n_clicks'))
-
 def update_output_div(input_value, n_clicks):
     if n_clicks is None:
         raise PreventUpdate
@@ -79,11 +100,16 @@ def update_output_div(input_value, n_clicks):
     input_value = input_value.strip().upper()
 
     if set(input_value) != set('ACTG'):
-        return html.P("Please input valid sequence containing only A, C, T, G", style={'color':'red'})
+        return html.P("Please input valid sequence containing only A, C, T, G",
+                      style={'color': 'red'})
 
     probability = infer(input_value, tokenizer, model)
     df['Probability'] = probability
-    fig = px.bar(df, x="Virus Type", y="Probability", color="Virus Type", title="Classification Result")
+    fig = px.bar(df,
+                 x="Virus Type",
+                 y="Probability",
+                 color="Virus Type",
+                 title="Classification Result")
     return [dcc.Graph(id='example-graph', figure=fig), update_embedding_viz(input_value)]
 
 
